377bb3b25b0f384c1bb1e6734cf4e67bb33f3699
Andrey Ivanin <andrey.ivanin@gmail.com>
2023-01-21 22:57:00 +0300

    fix json handler (v1)

--- internal/server/handlers/json.go
+++ internal/server/handlers/json.go
@@ -87,12 +87,37 @@ func MetricSummaryJSONHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 500)
 	}
 
-	jsonmetrics := []*Metrics{}
-	if err := json.Unmarshal(b, &jsonmetrics); err != nil {
-		log.Println(err)
-	}
+	if string(b)[0:1] == "[" {
+		jsonmetrics := []*Metrics{}
+		if err := json.Unmarshal(b, &jsonmetrics); err != nil {
+			log.Println(err)
+		}
+		for _, jsonmetric := range jsonmetrics {
+			switch jsonmetric.MType {
+			case "gauge":
+				if gmetric, err := storage.GetGMetric(jsonmetric.ID); err == nil {
+					jsonmetric.Value = &gmetric.Value
+				}
+			case "counter":
+				if cmetric, err := storage.GetCMetric(jsonmetric.ID); err == nil {
+					jsonmetric.Delta = &cmetric.Value
+				}
+			}
+		}
+		metricsJSON, err := json.Marshal(jsonmetrics)
 
-	for _, jsonmetric := range jsonmetrics {
+		if err != nil {
+			panic(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(metricsJSON)
+	} else {
+		jsonmetric := Metrics{}
+		if err := json.Unmarshal(b, &jsonmetric); err != nil {
+			log.Println(err)
+		}
 		switch jsonmetric.MType {
 		case "gauge":
 			if gmetric, err := storage.GetGMetric(jsonmetric.ID); err == nil {
@@ -103,15 +128,90 @@ func MetricSummaryJSONHandler(w http.ResponseWriter, r *http.Request) {
 				jsonmetric.Delta = &cmetric.Value
 			}
 		}
+
+		metricsJSON, err := json.Marshal(jsonmetric)
+
+		if err != nil {
+			panic(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(metricsJSON)
+
 	}
-
-	metricsJSON, err := json.Marshal(jsonmetrics)
-
-	if err != nil {
-		panic(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write(metricsJSON)
 }
+
+// jsonmetrics := []*Metrics{}
+
+// if err := json.Unmarshal(b, &jsonmetrics); err != nil {
+// 	log.Println(err)
+// }
+
+// 	for _, jsonmetric := range jsonmetrics {
+// 		switch jsonmetric.MType {
+// 		case "gauge":
+// 			if gmetric, err := storage.GetGMetric(jsonmetric.ID); err == nil {
+// 				jsonmetric.Value = &gmetric.Value
+// 			}
+// 		case "counter":
+// 			if cmetric, err := storage.GetCMetric(jsonmetric.ID); err == nil {
+// 				jsonmetric.Delta = &cmetric.Value
+// 			}
+// 		}
+// 	}
+
+// 	metricsJSON, err := json.Marshal(jsonmetrics)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write(metricsJSON)
+// }
+
+// type Slice []byte
+
+// type Result struct {
+// 	Key   string
+// 	Value []int
+// }
+
+// func (s *Slice) UnmarshalJSON(data []byte) error {
+// 	var obj map[string]json.RawMessage
+// 	if err := json.Unmarshal(data, &obj); err != nil {
+// 		return err
+// 	}
+
+// 	for key, raw := range obj {
+// 		r := Result{Key: key}
+// 		if raw[0] == '[' {
+// 			if err := json.Unmarshal(raw, &r.Value); err != nil {
+// 				return err
+// 			}
+// 		} else {
+// 			var i int
+//             if err := json.Unmarshal(raw, &i); err != nil {
+//                 return err
+//             }
+//             r.Value = append(r.Value, i)
+//         }
+// 		*s = append(*s, r)
+
+// 	}
+// 	return nil
+
+// }
+
+// func (s *Slice) UnmarshalJSON(data []byte) error {
+// 	if s[0] == "[" {
+// 		jsonmetrics := []*Metrics{}
+// 		if err := json.Unmarshal(s, jsonmetrics); err != nil {
+// 			return nil
+// 		}
+// 	} else {
+// 		jsonmetrics = *Metrics{}
+// 	}
+// }
