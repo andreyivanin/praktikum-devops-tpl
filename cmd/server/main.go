de643675896aae82388ed3fd63c9e3000f8449b7
Andrey Ivanin <andrey.ivanin@gmail.com>
2023-02-27 16:28:40 +0300

    fix signal (off)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,22 +2,18 @@ package main
 
 import (
 	"devops-tpl/internal/server"
-	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func main() {
-	termSignal := make(chan os.Signal, 1)
-	signal.Notify(termSignal, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	// termSignal := make(chan os.Signal, 1)
+	// signal.Notify(termSignal, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 
 	cfg := server.GetConfig()
 	storage := server.InitConfig(cfg)
 	r := server.NewRouter(storage)
 	http.ListenAndServe(cfg.Address, r)
 
-	sig := <-termSignal
-	log.Panicln("Finished, reason:", sig.String())
+	// sig := <-termSignal
+	// log.Panicln("Finished, reason:", sig.String())
 }
